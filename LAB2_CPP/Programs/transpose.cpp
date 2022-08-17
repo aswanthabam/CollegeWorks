5006eb16c0b679730b08d3a45d958b93be9d8256
aswanthabam <aswanth.abam@gmail.com>
2022-08-17 21:26:35 +0530

    Error in secom=nd program check it in Phone

--- LAB2_CPP/Programs/transpose.cpp
+++ LAB2_CPP/Programs/transpose.cpp
@@ -1,71 +1,61 @@
-// PROGRAM TO FIND THE TRACE AND TRANSPOSE OF A MATRIX
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Matrix
+class matrix
 {
-	int a[10][10],n;
-	public:
-	void get()
-	{
-		int temp;
-		cout<<"Enter order of square matrix: ";
-		cin>>n;
-		cout<<"Enter elements: \n";
-		// RECIVING MATRIX
-		for(int i = 0;i < n; i++)
-		{
-			for(int j = 0;j<n;j++)
-			{
-				cin>>temp;
-				a[i][j] =  temp;
-			}
-		}
-		// PRINTING MATRIX
-		cout<<"\nMatrix: \n|";
-		for(int i = 0;i < n+1;i++) cout<<"\t";
-		cout<<"|\n";
-		for(int i = 0;i < n; i++)
-		{
-			cout<<"|\t";
-			for(int j = 0;j<n;j++)
-			{
-				cout<<a[i][j]<<"\t";
-			}
-			cout<<"|\n|";
-			for(int i = 0;i < n+1;i++) cout<<"\t";
-			cout<<"|\n";
-		}
-	}
-	void trace()
-	{
-		int trace = 0;
-		for(int i = 0;i < n;i++)
-			for(int j = 0;j <n;j++)
-				if(i == j)
-					trace += a[i][j];
-		cout<<"\nTrace is "<<trace<<endl;
-		}
-	void transpose()
-	{
-		cout<<"\nTranspose Matrix is: \n|";
-		for(int i = 0;i < n+1;i++) cout<<"\t";
-		cout<<"|\n";
-		for(int i = 0;i < n;i++)
-		{
-			cout<<"|\t";
-			for(int j = 0;j <n;j++)
-				cout<<a[j][i]<<"\t";
-			cout<<"|\n|";
-			for(int i = 0;i < n+1;i++) cout<<"\t";
-			cout<<"|\n";
-	}
-	}
+ public:
+    int m, n, i, j, a[10][10], t[10][10], sum=0;
+    void getdata();
+    void trace();
+    void transpose();
 };
+void matrix::getdata()
+{
+    cout<<"\nEnter order of matrix : ";
+    cin>>m>>n;
+    cout<<"\nEnter array elements : "<<endl;
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+}
+void matrix::trace()
+{
+    if(m==n)
+    {
+       for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==j)
+                {
+                    sum=sum+a[i][j];
+                }
+            }
+        }
+        cout<<"\nTrace of the matrix is "<<sum<<endl<<endl;
+    }
+    else
+        cout<<"\nTrace cannot be found since the matrix is not a square matrix\n"<<endl;
+}
+void matrix::transpose()
+{
+    cout<<"\nTranspose of the matrix is"<<endl;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            cout<<a[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
-	Matrix m;
-	m.get();
-	m.transpose();
-	m.trace();
-	return 0;
+    matrix M;
+    M.getdata();
+    M.transpose();
+    M.trace();
 }
