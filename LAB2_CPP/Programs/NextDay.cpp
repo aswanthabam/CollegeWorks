4c85096f736bd2972bde0239b0be2e0273e56672
aswanthabam <aswanth.abam@gmail.com>
2022-11-10 14:41:30 +0530

    date program changes

--- LAB2_CPP/Programs/NextDay.cpp
+++ LAB2_CPP/Programs/NextDay.cpp
@@ -1,89 +1,94 @@
-//PROGRAM TO ADD ONE DAY TO A GIVEN DATE
+// FULL OF CORRECTIOns
 #include<iostream>
 using namespace std;
 class Date
-{ 
-  int d,m,y,flag = 0;
+{
 public:
-  void get()
+  int d,m,y,flag = 0;
+  void getDate()
   {
-    cout<<"Enter day, month and year: ";
+    cout<<"Enter date (DD MM YY): ";
     cin>>d>>m>>y;
-    cout<<"Entered date : "<<d<<"/"<<m<<"/"<<y<<endl;
   }
   void addDay()
   {
-    if(d > 31 || m > 12) flag = 1;
-    else
-    if(d > 27)
+    if(d > 31 || m > 12)
     {
-      if(m == 2)
+      flag = 1;
+    }
+    else{
+      if(d > 27)
       {
-        // MONTH IS FEBRUARY
-        if((y % 4 == 0 && y%100 == 0 && y%400==0) || (y%100 != 0 && y % 4 == 0))
-          if(d > 29)
+        if(m == 2)
+        {
+          // Fedruary
+          if(y % 400 == 0 || (y % 4 == 0 && y % 100 != 0))
           {
-            flag = 1;
+            // LEAP YEAR
+            if(d > 29) flag = 1;
+            else {
+              if(d == 29){
+                d = 1;
+                m++;
+              }else d++;
+            }
+          }else{
+            if(d > 28)
+            {
+              flag = 1;
+            }
+            else{
+              if(d == 28)
+              {
+                d = 1;
+                m++;
+              }else d++;
+            }
           }
-          else d++;
-        else{
-          if(d > 28)
-          {
-            flag = 1;
-          }
-          else
+        }
+        else if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10)
+        {
+          // MONTH Containing 31 days
+          if(d == 31)
           {
             d = 1;
             m++;
+          }else d++;
+        }
+        else if(m == 12)
+        {
+          // december
+          if(d == 31)
+          {
+            d = 1;
+            m = 1;
+            y ++;
+          }else d++;
+        }
+        else{
+          // MONTH CONTAINING 30days
+          if(d > 30) flag = 1;
+          else {
+            if(d == 30)
+            {
+              d = 1;
+              m++;
+            }else d++;
           }
         }
-      }
-      else if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10)
-      {
-        // MONTH CONTAINING 31 DAYS
-        if(d == 31)
-        {
-          d = 1;
-          m ++;
-        }else
-        {
-          d++;
-        }
-      }
-      else if(m == 12)
-      {
-        // MONTH IS DECEMBER
-        if(d == 31)
-        {
-          d = 1;
-          m = 1;
-          y++;
-        }else d++;
-      }
-      else{
-        // MONTH CONTAINING 30 DAYS
-        if(d>30) flag = 1;
-        if(d == 30)
-        {
-          d = 1;
-          m++;
-        }
-      }
+      }else d++;
     }
-    // DAY IS LESS THAN 28 NOTHING MORE WANT TO BE DONE.
-    else d++;
   }
   void display()
   {
-    if(flag == 0) cout<<"Next day is : "<<d<<"/"<<m<<"/"<<y<<endl;
-    else cout<<"Invalid Date!";
+    cout<<endl<<"Date is : "<<d<<"/"<<m<<"/"<<y<<endl;
   }
 };
 int main()
 {
-  Date date;
-  date.get();
-  date.addDay();
-  date.display();
+  Date d;
+  d.getDate();
+  d.addDay();
+  d.display();
   return 0;
 }
