264873d4472204ef98ccb860679ffcd05c6b5918
aswanthabam <aswanthabam@gmail.com>
2023-09-13 11:55:30 +0530

    none

---
+++ PYTHON/Programs/sort-des.py
@@ -0,0 +1,13 @@
+# sort a list in non descending order
+
+def srt(lst):
+  for i in range(len(lst) -1):
+    for i in range(len(lst) - i - 1):
+      if lst[i] < lst[i+1]:
+        lst[i],lst[i+1] = lst[i+1],lst[i]
+  for i in lst:
+    print(i,end=", ")
+  print()
+  
+lst = [int(i) for i in input("Enter list: ").split()]
+srt(lst)
